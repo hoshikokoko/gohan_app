d4d654b9e0b25abe2d26fcb5bc4699f6dcdafa96
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2022-11-17 03:47:16 +0000

    [Add]4-18 コメント機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
-  resources :post_images, only: [:index, :new, :create, :show, :destroy]
+  resources :post_images, only: [:index, :new, :create, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
- 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
