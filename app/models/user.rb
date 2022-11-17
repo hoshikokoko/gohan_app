fafe0b861c245667ceae70a967a6df8cf0a2278f
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2022-11-17 01:37:15 +0000

    [Add]4-14 usersモデル追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :post_images, dependent: :destroy
-   
+  
+  has_one_attached :profile_image
+  
+  def get_plofile_image(widh, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample_auther1.jpg')
+      plofile_image.attach(io: File.open(file_path), filename: 'default_image.jpg', content_type: 'image.jpeg')
+    end
+    profile_image_variant(resize_to_limit: [width, height]).processed
+  end
 end
